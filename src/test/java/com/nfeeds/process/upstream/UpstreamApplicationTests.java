124e5c3718718dde107a08921afb32bfbe793164
Leonardo Malcotti <leonardo.malcotti@studenti.unitn.it>
2023-01-30 15:31:06 +0100

    spring init

---
+++ src/test/java/com/nfeeds/process/upstream/UpstreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nfeeds.process.upstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpstreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
